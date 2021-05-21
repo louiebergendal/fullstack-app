4c23ba4c5f525c847cbdcf085c33bf41b536820c
louiebergendal <louie.bergendal@gmail.com>
2021-05-21 16:44:00 +0200

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
+		<Router>
 		<div className="App">
 			<header className="App-header">
-				I AM APP
+				<nav>
+					<Link to="/"> starting page </Link>
+					<Link to="/annan"> annan page </Link>
+				</nav>
 			</header>
+			<main>
+
+				<Switch>
+					<Route path="/annan"> I AM ANNAN </Route>
+					<Route path="/"> I AM START </Route>
+				</Switch>
+
+			</main>
 		</div>
+		</Router>
 	);
 }
 
