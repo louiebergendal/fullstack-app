97aab136bb168a82209c9116abb6b9cfc6e3c8cf
louiebergendal <louie.bergendal@gmail.com>
2021-05-22 13:02:15 +0200

    heroku setup. Missade tyvärr private key i initial heroku push.

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+// const hamsters = require('./routes/hamsters.js')
+
+const PORT = process.env.PORT || 1340
+
+const buildFolder = path.join(__dirname, '../build')
+
+// ============= MIDDLEWARE ============= //
+
+// Logger - skriv ut info om inkommande request
+app.use((req, res, next) => {
+	console.log('===============');
+	console.log(`${req.method}  ${req.url} `, req.params);
+	next()
+})
+
+app.use( express.json() )
+app.use( cors() )
+app.use( express.static(buildFolder) )
+// app.use( express.static(buildImgFolder) )
+
+// ============= ROUTES ============= //
+
+// app.use('/hamsters', hamsters)
+app.get('/', (req, res) => {
+    res.send('Hello from server')
+})
+
+// Ska vara sist för att fånga upp alla övriga requests
+// För att frontend-rountingen ska fungera
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
+// ============= RESTEN ============= //
+
+// Felhanterare
+app.use(errorHandler)
+function errorHandler (err, req, res, next) {
+
+	console.log('HOPPSAN!');
+	
+	if (res.headersSent) {
+	  return next(err)
+	}
+	res.status(500)
+	res.render('error', { error: err })
+}
+
+// Starta servern
+app.listen(PORT, () => {
+	console.log('Server listening on port ' + PORT);
+})
--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
+/*
+	TO DO:
+		- Kolla alla filmerna (1 - 4 på skrivbordet)
+			x film 1 - basic app med frontend-routing
+			- film 2 - kopiera backend (23:26)
+			- film 3
+			- film 4
+
+	PÅMÅNDAG:
+		- Fixa globalspace
+
+
+	SETUP:
+		- Stå i projektmappen	
+			"npm run build"
+			"node backend/server.js" - backend
+			"npm run start" - frontend
+
+
+	BAJS:
+		- Jag pushade till Heroku, innan jag lagt till en private key.
+		- Jag hittar ingen private key.
+		- Hur skaffar jag en private key?
+*/
+
 function App() {
 	return (
 		<Router>
